2c636754537535ea9e7af789e80324742dd7ee2c
Afaf Afridi <ibrahim.hasan00710@gmail.com>
2025-07-05 22:18:35 +0500

    Temperature commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let temp=prompt("Enter current temperature in your city");
+if(temp<0){
+    alert("It's freezing outside");
+    console.log("It's freezing outside"); 
+}else if(temp<10&&temp>=20){
+    alert("Its cold outside");
+    console.log("It's cold outside");
+}else if(temp<=40&&temp>=30){
+    alert("It's hot outside");
+    console.log("It's hot outside");
+}else if(temp>=50&&temp<60){
+    alert("Coldest day in Arizona fr");
+    console.log("Coldest day in Arizona fr");
+}else if(isNaN(temp)){
+    alert("Please enter a valid number");
+    console.log("Please enter a valid number");
+}else{
+    alert("It's a nice day outside");
+    console.log("It's a nice day outside");
+}
